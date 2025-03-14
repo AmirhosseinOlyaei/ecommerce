130cd67483700f237a1af2ba3c8bbcf3aa967081
Amirhossein Olyaei <amirholyaei@gmail.com>
2025-03-14 15:19:33 -0400

    refactor: enhance products listing page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { CartPreviewButton } from "@/components/cart/CartPreviewButton"
 import { ProductsClientPage } from "@/components/products/ProductsClientPage"
 import { Button, HomeIcon, ListIcon } from "@/components/ui/Button"
 import { useRouter } from "next/navigation"
@@ -8,13 +9,14 @@ export default function ProductsPage() {
   const router = useRouter()
 
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100 dark:bg-gray-900">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
       <div className="container p-6 mx-auto max-w-7xl">
-        <div className="flex justify-between items-center mb-8">
+        <div className="flex items-center justify-between mb-8">
           <h1 className="text-3xl font-bold text-gray-900 dark:text-gray-100">
             Products
           </h1>
           <div className="flex items-center space-x-4">
+            <CartPreviewButton />
             <Button
               onClick={() => router.push("/dashboard")}
               variant="secondary"
@@ -31,7 +33,7 @@ export default function ProductsPage() {
             </Button>
           </div>
         </div>
-        <div className="p-6 mb-6 max-w-7xl bg-white rounded-lg shadow dark:bg-gray-800 dark:shadow-gray-700">
+        <div className="p-6 mb-6 bg-white rounded-lg shadow max-w-7xl dark:bg-gray-800 dark:shadow-gray-700">
           <ProductsClientPage />
         </div>
       </div>
